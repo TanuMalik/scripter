3e8a33587e668e9c2166321d0bf602db396f71a0
Zhihao Yuan <lichray@gmail.com>
2017-08-10 00:49:43 -0500

    chore: update copyrights and usage

--- scripter.c
+++ scripter.c
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2017  Zhihao Yuan
  * Copyright (c) 2010, 2012  David E. O'Brien
  * Copyright (c) 1980, 1992, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -11,9 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -246,7 +244,8 @@ static void
 usage(void)
 {
 	(void)fprintf(stderr,
-	    "usage: script [-adfkpqr] [-t time] [file [command ...]]\n");
+	    "usage: %s [-aiq] [-F pipe] [-t time] [file [command ...]]\n",
+	    getprogname());
 	exit(1);
 }
 
