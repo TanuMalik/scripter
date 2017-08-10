300f1259d62c148e5e1b06c809c5ba60d751742e
Zhihao Yuan <lichray@gmail.com>
2017-08-10 11:53:04 -0500

    feat: remove outdated ident(1) information

--- scripter.c
+++ scripter.c
@@ -26,16 +26,7 @@
  * SUCH DAMAGE.
  */
 
-#include <sys/param.h>
-#ifndef lint
-static const char copyright[] =
-"@(#) Copyright (c) 1980, 1992, 1993\n\
-	The Regents of the University of California.  All rights reserved.\n";
-#endif
-#ifndef lint
-static const char sccsid[] = "@(#)script.c	8.1 (Berkeley) 6/6/93";
-#endif
-
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
@@ -87,7 +78,7 @@ main(int argc, char *argv[])
 	time_t tvec, start;
 	char obuf[BUFSIZ];
 	fd_set rfd;
-	int aflg, Fflg, kflg, ch, k, n;
+	int aflg, Fflg, ch, k, n;
 	int flushtime, readstdin;
 
 	aflg = Fflg = 0;
